563f996d6e3dae7c8866000aaff83e3b48b00c3e
Gaurav Panchal <gaurav.panchal@sooryen.com>
2016-09-28 14:29:44 +0530

    adding angular app.js

---
+++ src/views/app.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app', [
+            'ui.bootstrap',
+            'ui.router',
+            'ngCookies'
+        ])
+        .constant('CONFIG',{});
+})();
